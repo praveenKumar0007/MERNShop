afa8bfb6d80b3a4aad0fc0912f9c9a0945108b42
PraveenKumar <praveenlog17122001@gmail.com>
2022-06-07 09:35:18 +0530

    React Router Setup and A to Link Tag

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -10,11 +10,11 @@ const Product = ({ product }) => {
         <Card.Img src={product.image} variant='top' />
       </Link>
       <Card.Body>
-        <a href={`product/${product._id}`}>
+        <Link to={`product/${product._id}`}>
           <Card.Title as='div'>
             <strong>{product.name}</strong>
           </Card.Title>
-        </a>
+        </Link>
         <Card.Text as='div'>
           <Rating
             value={product.rating}
