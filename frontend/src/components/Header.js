402f17eaf7ff681941012a21cda8e570282ff611
PraveenKumar <praveenlog17122001@gmail.com>
2022-06-12 11:11:38 +0530

    Product Screen Setup

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -15,12 +15,12 @@ const Header = () => {
             <Nav className='ml-auto'>
               <LinkContainer to='/cart'>
                 <Nav.Link>
-                  <i className='fas fa-shopping-cart'>Cart</i>
+                  <i className='fas fa-shopping-cart'> Cart</i>
                 </Nav.Link>
               </LinkContainer>
               <LinkContainer to='/login'>
                 <Nav.Link>
-                  <i className='fas fa-user'>Sign In</i>
+                  <i className='fas fa-user'> Sign In</i>
                 </Nav.Link>
               </LinkContainer>
             </Nav>
